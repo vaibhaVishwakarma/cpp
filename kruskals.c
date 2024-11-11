5bb63e7bb242c3052becba9059001c0c048e644f
vaibhaVishwakarma <164744837+vaibhaVishwakarma@users.noreply.github.com>
2024-11-11 21:51:50 +0530

    Create kruskals.c

---
+++ kruskals.c
@@ -0,0 +1,166 @@
+//KRUSKALS
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+#define scan(n) scanf("%d",&(n))
+#define print(n) printf("%d ",n)
+#define NIL {0,0,0}
+#include<limits.h>
+int V =0   , E = 0;
+
+typedef struct{
+    int first , second;
+}pair;
+typedef struct{
+    int first , second,third;
+}tri;
+
+typedef tri qnode;
+typedef struct QQ{
+    qnode arr[MAX];
+    int f , r;
+}q;
+void swap(tri* q1 , tri* q2){
+    tri t = *q1;
+    *q1 = *q2;
+    *q2 = t;
+}
+void swap(pair* q1 , pair* q2){
+    pair t = *q1;
+    *q1 = *q2;
+    *q2 = t;
+}
+void swap(int* q1 , int* q2){
+    int t = *q1;
+    *q1 = *q2;
+    *q2 = t;
+}
+void swap(q** q1 , q** q2){
+    q* t = *q1;
+    *q1 = *q2;
+    *q2 = t;
+}
+void init(q* queue){
+    queue->f = -1;
+    queue->r = -1;
+}
+int isFull(q* queue){
+    return queue->f == (queue->r+1)%MAX;}
+int isEmpty(q* queue){
+    return queue->f == -1 || queue->r == -1;}
+int isSingular(q* queue){
+    return queue->f == queue->r ;}
+int increase(int i ){return (i+1)%MAX;}
+void sortq(q*queue){
+    for(int i = queue->f ; i != queue->r ; i = increase(i)){
+        int mni = i;
+        for(int j = (i+1)%MAX ;  j != (queue->r+1)%MAX ; j = increase(j)){
+            if(queue->arr[mni].second > queue->arr[j].second)  
+            // && queue->arr[mni].third > queue->arr[j].third)
+                mni = j;
+            
+            
+        }
+        if(i!=mni) swap(&queue->arr[i] , &queue->arr[mni]);
+    }
+}
+    
+void push(q* queue , qnode newnode){
+    if(isFull(queue)){printf("full\n"); return;}
+    if(isEmpty(queue)) queue->f = 0;
+    queue->r = (queue->r+1)%MAX;
+    queue->arr[queue->r] = newnode;
+    // sortq(queue);
+} //priority queue
+qnode pop(q* queue ){
+    if(isEmpty(queue)){printf("empty\n"); return NIL;}
+    qnode ret = queue->arr[queue->f];
+    queue->arr[queue->f] = NIL;
+    if(isSingular(queue))init(queue);
+    else queue->f = (queue->f+1)%MAX;
+    return ret;}
+
+void printqf(q* queue ){
+    if(isEmpty(queue)) return;
+    for(int i = queue->f ; i!=queue->r ; i = increase(i))
+        print(queue->arr[i].first);
+    print(queue->arr[queue->r].first);
+    printf("\n");
+}
+void printq(q* queue ){
+    if(isEmpty(queue)) return;
+    for(int i = queue->f ; i!=queue->r ; i = increase(i))
+        print(queue->arr[i].second);
+    print(queue->arr[queue->r].second);
+    printf("\n");
+}
+int getsum(int* arr){
+    int sm = 0;
+    for(int i = 0 ; i <V ; i++) sm+= arr[i];
+    return sm;
+}
+
+int main(void){
+    
+    scan(V);
+    scan(E);
+    int adj[V][V];
+    for(int i = 0 ; i<V ; i++) {
+    for(int j = 0 ; j<V ; j++) adj[i][j] = 0; }
+    for(int i = 0 ; i<E ; i++) {
+            int r, c;
+            scan(r);
+            scan(c);
+            scan(adj[r][c]);
+            // adj[c][r] = adj[r][c];
+        
+        }
+    // for(int i = 0 ; i<V ; i++) {
+        // for(int j = 0 ; j<V ; j++) print(adj[i][j]); printf("\n");}
+    for(int i = 0 ; i<V ; i++) {
+        for(int j = 0 ; j<V ; j++) if(adj[i][j] == 0 ) adj[i][j] = INT_MAX;}
+    //input ends here
+    
+    
+    // int parent[V]; for(int i = 0 ; i<V ; i++) parent[i] = -1;
+    int visited[V]; for(int i = 0 ; i<V ; i++) visited[i] = 0;
+    
+    q qq , qq2 ; q* que = &qq , *que2 = &qq2; init(que);init(que2);
+    
+    push(que , {0,0,0}); // orgin -> o , distance = 0;
+    // parent[0] = 0;
+    int vis = 0;
+    int found = 1;
+ 
+    // visited[0] = 1;
+    
+    int wt = 0;
+    
+    for(int i = 0 ; i<V ; i++){
+        for(int v = 0 ; v<V ; v++){
+            if(adj[i][v] == INT_MAX || i == v) continue;
+            push(que,{v,adj[i][v],i});
+        }
+    }
+    printf("Edges in the constructed MST:\n");
+    while(getsum(visited)<V){
+        sortq(que);
+        tri it = pop(que);
+        int curr = it.first , parent = it.third , weight = it.second;
+        if(curr == parent) continue;
+        if(visited[curr] && visited[parent]) continue;
+        visited[curr] = 1;
+        visited[parent]= 1;
+        wt += weight;
+        printf("%d -- %d == %d\n",parent , curr , weight);
+    }
+    printf("Minimum Spanning Tree cost: %d",wt);
+    
+      
+    return 0;
+    
+}
+    
+
+
+ 
